5eb868e48ddf0fa776cb58a403f862d9218be22b
Engin-Ozlem <ozlemengin135@gmail.com>
2023-04-01 13:36:22 +0300

    project created

---
+++ FitKal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitKal
+//
+//  Created by macbookair on 1.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
