13ea2855907bef8946410da5720c50ad58be9729
Engin-Ozlem <ozlemengin135@gmail.com>
2023-04-01 15:39:06 +0300

    pods uploaded

---
+++ FitKal/ViewControllers/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitKal
+//
+//  Created by macbookair on 1.04.2023.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
